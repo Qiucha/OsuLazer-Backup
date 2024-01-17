e3cee9819d4544428e83d13790199df4c3f6f298
Yun-Hsuan, Ku <56359864+Qiucha@users.noreply.github.com>
2024-01-17 18:36:31 +0800

    update comments for two script files via upload

--- OsulazerBackup.ps1
+++ OsulazerBackup.ps1
@@ -1,8 +1,15 @@
+# Ask for backup path
+# Set usful vars
 
 $bk_path = Read-Host -Prompt "Paste your backup path"
 $uname = $env:USERNAME
 $bk_file_Local = $bk_path+"\osulazer.zip"
 $bk_file_Roaming = $bk_path+"\osu.zip"
+
+
+# Extract two zip files to respective path
+# NOTE that these commands include -Force option
+# Which would overwrite the existing zip files.
 
 Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
 
--- OsulazerImport.ps1
+++ OsulazerImport.ps1
@@ -1,8 +1,16 @@
+# Ask for backup path
+# Set usful vars
 
 $bk_path = Read-Host -Prompt "Paste your backup path"
 $uname = $env:USERNAME
 $bk_file_Local = $bk_path+"\osulazer.zip"
 $bk_file_Roaming = $bk_path+"\osu.zip"
+
+# Extract two zip files to respective path
+# NOTE that these commands include -Force option
+# Which would overwrite the existing files under two path below:
+# %USERNAME%\AppData\Local\osulazer\
+# %USERNAME%\AppData\Roaming\osu\
 
 Expand-Archive -Force -LiteralPath $bk_file_Local -DestinationPath "C:\Users\$uname\AppData\Local\"
 
