6fbd0e3c7178def1e7afc600bd6e52c65a906bd7
Yun-Hsuan, Ku <56359864+Qiucha@users.noreply.github.com>
2024-01-18 17:36:09 +0800

    v1 update

--- OsulazerBackup.ps1
+++ OsulazerBackup.ps1
@@ -1,7 +1,31 @@
-# Ask for backup path
-# Set useful vars
+### NOTE: This Backup method would save THE WHOLE SESSION as two zips
+### including maps, skins, songs, replays, collections, and also LOGIN STATUS.
+### DO NOT SHARE THE ZIP FILES WITH OTHER PLAYER ONLY EXPECTING SHARING YOUR SONG COLLECTION !!!
 
-$bk_path = Read-Host -Prompt "Paste your backup path"
+
+# Set Execution policy temporarily
+
+$defaultPolicy = Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+
+
+# Ask for backup path
+# Paste your back up path HERE between "" 
+
+#---for example: $bk_path = "D:\Backup"---#
+$bk_path = ""
+#---
+
+
+# If not set, ask for user input at first
+
+if ( "" -eq $bk_path ){
+    $bk_path = Read-Host -Prompt "Paste your backup path"
+}
+
+
+# Set usful vars
+
 $uname = $env:USERNAME
 $bk_file_Local = $bk_path+"\osulazer.zip"
 $bk_file_Roaming = $bk_path+"\osu.zip"
@@ -11,6 +35,16 @@ $bk_file_Roaming = $bk_path+"\osu.zip"
 # NOTE that these commands include -Force option
 # Which would overwrite the existing zip files.
 
-Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
+try{
+	7z a -r $bk_file_Local "C:\Users\$uname\AppData\Local\osulazer\*"
+	7z a -r $bk_file_Roaming "C:\Users\$uname\AppData\Roaming\osu\*"
+}
+catch{
+	Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
+	Compress-Archive -Force -Path "C:\Users\$uname\AppData\Roaming\osu\"-DestinationPath $bk_file_Roaming
+}
 
-Compress-Archive -Force -Path "C:\Users\$uname\AppData\Roaming\osu\"-DestinationPath $bk_file_Roaming
+
+# Set ExecutionPolicy Back to default
+
+Set-ExecutionPolicy -ExecutionPolicy $defaultPolicy -Scope Process
--- OsulazerImport.ps1
+++ OsulazerImport.ps1
@@ -1,10 +1,35 @@
-# Ask for backup path
-# Set useful vars
+### NOTE: This Import method would load THE WHOLE SESSION as two zips
+### including maps, skins, songs, replays, collections, and also LOGIN STATUS.
+### DO NOT SHARE THE ZIP FILES WITH OTHER PLAYER ONLY EXPECTING SHARING YOUR SONG COLLECTION !!!
 
-$bk_path = Read-Host -Prompt "Paste your backup path"
+
+# Set Execution policy temporarily
+
+$defaultPolicy = Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+
+
+# Ask for backup path
+# Paste your back up path HERE between "" 
+
+#---for example: $bk_path = "D:\Backup"---#
+$bk_path = ""
+#---
+
+
+# If not set, ask for user input at first
+
+if ( "" -eq $bk_path ){
+    $bk_path = Read-Host -Prompt "Paste your backup path"
+}
+
+
+# Set usful vars
+
 $uname = $env:USERNAME
 $bk_file_Local = $bk_path+"\osulazer.zip"
 $bk_file_Roaming = $bk_path+"\osu.zip"
+
 
 # Extract two zip files to respective path
 # NOTE that these commands include -Force option
@@ -12,6 +37,15 @@ $bk_file_Roaming = $bk_path+"\osu.zip"
 # %USERNAME%\AppData\Local\osulazer\
 # %USERNAME%\AppData\Roaming\osu\
 
-Expand-Archive -Force -LiteralPath $bk_file_Local -DestinationPath "C:\Users\$uname\AppData\Local\"
+try{
+	7z x $bk_file_Local -y -o"C:\Users\$uname\AppData\Local\osulazer\"
+	7z x $bk_file_Roaming -y -o"C:\Users\$uname\AppData\Roaming\osu\"
+}
+catch{
+	Expand-Archive -Force -LiteralPath $bk_file_Local -DestinationPath "C:\Users\$uname\AppData\Local\"
+	Expand-Archive -Force -LiteralPath $bk_file_Roaming -DestinationPath "C:\Users\$uname\AppData\Roaming\"
+}
 
-Expand-Archive -Force -LiteralPath $bk_file_Roaming -DestinationPath "C:\Users\$uname\AppData\Roaming\"
+
+# Set ExecutionPolicy Back to default
+Set-ExecutionPolicy -ExecutionPolicy $defaultPolicy -Scope Process
