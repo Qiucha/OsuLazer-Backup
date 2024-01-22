7d623ac27aeaccf4f2eb9a29434b558fd364a4f5
Yun-Hsuan, Ku <56359864+Qiucha@users.noreply.github.com>
2024-01-23 01:11:02 +0800

    Adding back missing }}

--- OsulazerBackup.ps1
+++ OsulazerBackup.ps1
@@ -44,6 +44,8 @@ if (!(Test-Path -Path $bk_file_Local)){
 	catch{
 		Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
 		Compress-Archive -Force -Path "C:\Users\$uname\AppData\Roaming\osu\"-DestinationPath $bk_file_Roaming
+	}
+}
 	
 # Set ExecutionPolicy Back to default
 
