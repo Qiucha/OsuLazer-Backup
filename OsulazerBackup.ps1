141f680c32b2ac7654bcadc89e416c3447b21e37
Yun-Hsuan, Ku <56359864+Qiucha@users.noreply.github.com>
2024-01-23 01:05:14 +0800

    Updated some error catching

--- OsulazerBackup.ps1
+++ OsulazerBackup.ps1
@@ -23,28 +23,28 @@ if ( "" -eq $bk_path ){
     $bk_path = Read-Host -Prompt "Paste your backup path"
 }
 
+if (!(Test-Path -Path $bk_file_Local)){
+	Read-Host -Prompt "Path not found, please check your path. Press enter to exit..."
+} else {
+	# Set usful vars
 
-# Set usful vars
-
-$uname = $env:USERNAME
-$bk_file_Local = $bk_path+"\osulazer.zip"
-$bk_file_Roaming = $bk_path+"\osu.zip"
-
-
-# Extract two zip files to respective path
-# NOTE that these commands include -Force option
-# Which would overwrite the existing zip files.
-
-try{
-	7z a -r $bk_file_Local "C:\Users\$uname\AppData\Local\osulazer\*"
-	7z a -r $bk_file_Roaming "C:\Users\$uname\AppData\Roaming\osu\*"
-}
-catch{
-	Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
-	Compress-Archive -Force -Path "C:\Users\$uname\AppData\Roaming\osu\"-DestinationPath $bk_file_Roaming
-}
+	$uname = $env:USERNAME
+	$bk_file_Local = $bk_path+"\osulazer.zip"
+	$bk_file_Roaming = $bk_path+"\osu.zip"
 
 
+	# Extract two zip files to respective path
+	# NOTE that these commands include -Force option
+	# Which would overwrite the existing zip files.
+
+	try{
+		7z a -r $bk_file_Local "C:\Users\$uname\AppData\Local\osulazer\*"
+		7z a -r $bk_file_Roaming "C:\Users\$uname\AppData\Roaming\osu\*"
+	}
+	catch{
+		Compress-Archive -Force -Path "C:\Users\$uname\AppData\Local\osulazer\" -DestinationPath $bk_file_Local
+		Compress-Archive -Force -Path "C:\Users\$uname\AppData\Roaming\osu\"-DestinationPath $bk_file_Roaming
+	
 # Set ExecutionPolicy Back to default
 
 Set-ExecutionPolicy -ExecutionPolicy $defaultPolicy -Scope Process
--- OsulazerImport.ps1
+++ OsulazerImport.ps1
@@ -29,23 +29,35 @@ if ( "" -eq $bk_path ){
 $uname = $env:USERNAME
 $bk_file_Local = $bk_path+"\osulazer.zip"
 $bk_file_Roaming = $bk_path+"\osu.zip"
+$lazerzip_exist = Test-Path -Path $bk_file_Local
+$osuzip_exist = Test-Path -Path $bk_file_Roaming
 
 
-# Extract two zip files to respective path
-# NOTE that these commands include -Force option
-# Which would overwrite the existing files under two path below:
-# %USERNAME%\AppData\Local\osulazer\
-# %USERNAME%\AppData\Roaming\osu\
+# Error Catch for zip files not exist
+# If not exist either file, Require user input.
 
-try{
-	7z x $bk_file_Local -y -o"C:\Users\$uname\AppData\Local\osulazer\"
-	7z x $bk_file_Roaming -y -o"C:\Users\$uname\AppData\Roaming\osu\"
+if ($lazerzip_exist -And $osuzip_exist){
+	# Extract two zip files to respective path
+	# NOTE that these commands include -Force option
+	# Which would overwrite the existing files under two path below:
+	# %USERNAME%\AppData\Local\osulazer\
+	# %USERNAME%\AppData\Roaming\osu\
+
+	try{
+		7z x $bk_file_Local -y -o"C:\Users\$uname\AppData\Local\osulazer\"
+		7z x $bk_file_Roaming -y -o"C:\Users\$uname\AppData\Roaming\osu\"
+	}
+	catch{
+		Expand-Archive -Force -LiteralPath $bk_file_Local -DestinationPath "C:\Users\$uname\AppData\Local\"
+		Expand-Archive -Force -LiteralPath $bk_file_Roaming -DestinationPath "C:\Users\$uname\AppData\Roaming\"
+	}
 }
-catch{
-	Expand-Archive -Force -LiteralPath $bk_file_Local -DestinationPath "C:\Users\$uname\AppData\Local\"
-	Expand-Archive -Force -LiteralPath $bk_file_Roaming -DestinationPath "C:\Users\$uname\AppData\Roaming\"
+else{
+	Write-output "More than one zip files missing, check your path!"
+	Read-Host -Prompt "Press Enter key to exit..."
 }
 
 
 # Set ExecutionPolicy Back to default
+
 Set-ExecutionPolicy -ExecutionPolicy $defaultPolicy -Scope Process
